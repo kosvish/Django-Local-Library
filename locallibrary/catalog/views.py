a62c37be1d4ea73eba358db56bb1b303efd94624
mrkoc <mr.kocta@bk.ru>
2024-05-21 11:10:37 +0300

    Create view controller, get all models quantity. Return render html template

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Book, BookInstance, Author, Language, Genre
 
-# Create your views here.
+
+def index(request):
+    """
+    Функция для отображения для домашней страницы сайта
+    :param request:
+    :return:
+    """
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги в статусе "a"
+    available_genres = Genre.objects.all().count()
+    # num_books_with_word =
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    return render(request,'index.html', context={
+        'num_books': num_books, 'num_instances': num_instances, 'num_instance_available': num_instances_available,
+        'num_authors': num_authors
+    })
