b6957db1db23c17db4b2b8feefec40a555edde68
mrkoc <mr.kocta@bk.ru>
2024-05-24 16:20:25 +0300

    create class for list books, and book detail

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, BookInstance, Author, Language, Genre
+from django.views import generic
 
 
 def index(request):
@@ -16,7 +17,20 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
 
-    return render(request,'index.html', context={
+    return render(request, 'index.html', context={
         'num_books': num_books, 'num_instances': num_instances, 'num_instance_available': num_instances_available,
         'num_authors': num_authors
     })
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+    # def get_queryset(self):
+    #     return
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
