a8add9750e000c087bd4cc5b44ab67335d108b00
mrkoc <mr.kocta@bk.ru>
2024-05-26 15:01:21 +0300

    (add): work with session, adding how much user visited main page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -13,13 +13,14 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     # Доступные книги в статусе "a"
     available_genres = Genre.objects.all().count()
-    # num_books_with_word =
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     return render(request, 'index.html', context={
         'num_books': num_books, 'num_instances': num_instances, 'num_instance_available': num_instances_available,
-        'num_authors': num_authors
+        'num_authors': num_authors, "num_visits": num_visits
     })
 
 
