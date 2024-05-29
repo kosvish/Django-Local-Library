c2d07010dd566e127e64ce59010752b733f8cabf
mrkoc <mr.kocta@bk.ru>
2024-05-29 17:58:50 +0300

    (create): views for create, update, delete authors also create view to validate incoming data and if request method is POST, saving bookinst, else generate new form

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from .models import Book, BookInstance, Author, Language, Genre
+from .models import Book, BookInstance, Author, Genre
 from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import datetime
+from .forms import RenewBookForm
+from django.contrib.auth.decorators import permission_required
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 def index(request):
@@ -24,6 +32,30 @@ def index(request):
     })
 
 
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    book_inst = get_object_or_404(BookInstance, pk=pk)
+    # Если данный метод это POST запрос, тогда
+    if request.method == "POST":
+        # создаем экземпляр формы и заполняем данными из запроса (связывание)
+        form = RenewBookForm(request.POST)
+        # Проверка подлинности формы
+        if form.is_valid():
+            # обработка данных из form.cleaned_data
+            # здесь просто присваем данные формы их полю due_back
+            book_inst.due_back = form.cleaned_data["renewal_date"]
+            book_inst.save()
+
+            # Переход по новому адрессу
+            return HttpResponseRedirect(reverse('index'))
+    # Если это GET запрос, создать форму по умолчанию
+    else:
+        proposed_renewal_date = datetime.datetime.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={"renewal_date": proposed_renewal_date})
+        return render(request, 'catalog/book_renew_librarian.html',
+                      {"form": form, "bookinst": book_inst})
+
+
 class BookListView(generic.ListView):
     model = Book
 
@@ -34,4 +66,19 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
 
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial = {"date_of_birt": "12/10/2020"}
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', "last_name", 'date_of_birth', "date_of_death"]
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
 
